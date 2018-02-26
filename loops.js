eee8128ca902f2ab79e0cf5042d00b4bf8766963
Kaushal Patel <kaushalpatel778@gmail.com>
2018-02-26 02:29:41 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,4 +1,4 @@
-function forLoop(array) {
+
 /*
     for(i = 0; i < 25, i++){
       if (i === 1){
@@ -23,22 +23,3 @@ function forLoop(array) {
   return array;
 }
 
-
-function whileLoop(number) {
-  while (number > 0) {
-    console.log(--number)
-  }
-  return "done";
-}
-
-function maybeTrue() {
-  return Math.random() >= 0.5
-}
-
-function doWhileLoop(array) {
-  do {
-    array.pop();
-  }
-  while (array.length > 0 && maybeTrue());
-  return array;
-}
